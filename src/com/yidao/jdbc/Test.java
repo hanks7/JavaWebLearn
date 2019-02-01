74621dc41b385dbadd1c09d4cf64d5bfc1a53976
hanks7 <474664736@qq.com>
2019-02-01 18:25:01 +0800

    笔记本提交

--- src/com/yidao/jdbc/Test.java
+++ src/com/yidao/jdbc/Test.java
@@ -1,14 +1,9 @@
 package com.yidao.jdbc;
 
 import com.hanks.test.TestLog;
-import com.yidao.jdbc.bean.BaseBean;
 import com.yidao.jdbc.uitls.HttpUtils;
 import com.yidao.jdbc.uitls.Ulog;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
 
 public class Test {
 
@@ -17,7 +12,7 @@ public class Test {
         String message = HttpUtils.sendGetRequest("http://116.247.74.76:8682/api/AutoUpdate?id=00000000000001", null);
         Ulog.i(message);
         Ulog.i("网络请求所用时间:", (System.currentTimeMillis() - privus));
-        TestLog.i("fkldjasf");
+        TestLog.i("测试引入的module");
     }
 
 }
