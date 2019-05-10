e080899081464b272aef0efb077493b0cc1a11f8
hjj <474664736@qq.com>
2019-05-10 17:20:41 +0800

    github测试提交

---
+++ web/html/html-js/test.js
@@ -0,0 +1,21 @@
+﻿/**
+ * 工具库
+ * author==== 侯建军
+ * date==== 2018/12/1
+ */
+var log = {
+    i: function (tag, content) {
+        if (content === undefined) {
+            console.log(tag);
+        } else {
+            console.log(tag + "=" + content);
+        }
+    },
+    w: function (tag, content) {
+        if (content === undefined) {
+            document.writeln(tag + "<br/><br/>");
+        } else {
+            document.writeln(tag + "=" + content + "<br/><br/>");
+        }
+    }
+}
