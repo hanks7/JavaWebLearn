690a23b35c62fdd0044ab9d2e0dd40603e8b25c5
hjj <474664736@qq.com>
2020-01-19 16:43:34 +0800

    1.添加Ajax 和 jQuery

--- src/com/yidao/jdbc/imooc/day1Servlet/direct/CheckLoginServlet.java
+++ src/com/yidao/jdbc/imooc/day1Servlet/direct/CheckLoginServlet.java
@@ -13,11 +13,12 @@ import java.io.IOException;
 public class CheckLoginServlet extends HttpServlet {
 
 	/**
-	 * 转发 只是servlet中调用url,并不是浏览器调用
-	 * 重定向则是浏览器去调用url 而且要加上contextPath(在这个项目就是"/jdbc") 浏览器中的url肯定会发生变化.在加上本身也是Servlet也是通过url调用的
+	 * 1.转发 只是servlet中调用url,并不是浏览器调用
+	 *
+	 * 2.重定向则是浏览器去调用url 而且要加上contextPath(在这个项目就是"/jdbc") 浏览器中的url肯定会发生变化.在加上本身也是Servlet也是通过url调用的
 	 * 所以浏览器会调用两次url这么一说(注:并不是两个相同的url,课程中有误解的嫌疑)
 	 *
-	 * 如果用转发，可以直接用request、response。如果用重定向就只能用session 来传值
+	 * 3.如果用转发，可以直接用request、response。如果用重定向就只能用session 来传值
 	 *
 	 * @param request
 	 * @param response
---
+++ src/com/yidao/jdbc/test/servlet/NewsListServlet.java
@@ -0,0 +1,44 @@
+package com.yidao.jdbc.test.servlet;
+
+import com.alibaba.fastjson.JSON;
+import com.yidao.jdbc.bean.News;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/NewsListServlet")
+public class NewsListServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String type = request.getParameter("t");
+        List<News> list = null;
+        if (type != null && type.equals("pypl")) {
+            list = getNews(type);
+        }else{
+            list = getNews("未填写");
+        }
+
+
+        String json = JSON.toJSONString(list);
+        response.setContentType("text/html;charset=utf-8");                    //HTML格式
+        response.getWriter().write(json);
+
+    }
+
+    private List<News> getNews(String type) {
+        List<News> list = new ArrayList<>();
+        list.add(new News(type, type + "2020年1月19日15:34:13", "新华社", type + "新华社 aaaaaaaa"));
+        list.add(new News(type, type + "2020年1月19日15:34:13", "人民日报", type + "人民日报 bbbaaaaaaa"));
+        list.add(new News(type, type + "2020年1月19日15:34:13", "环球日报", type + "环球日报 cccaaaaaaa"));
+        return list;
+    }
+}
